dc84b8a36f37f6a71c04647edf90b47bcaec81db
Kaustubh Sable <kaustubhsable05@gmail.com>
2020-04-11 20:27:35 -0400

    Create Solution.cpp

---
+++ 30-Day-Challenge/Day1/Solution.cpp
@@ -0,0 +1,11 @@
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+using namespace std;
+int main()
+{
+	//Write code here
+	string name;
+	getline(cin,name);
+	cout<<"Hello Techgig"<<endl;
+	cout<<name;
+}
