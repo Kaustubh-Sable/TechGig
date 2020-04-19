f09e124ec64514b8ed117084eb8717d073fee9f1
Kaustubh Sable <kaustubhsable05@gmail.com>
2020-04-18 20:23:38 -0400

    Create Solution.cpp

---
+++ 30-Day-Challenge/Day16/Solution.cpp
@@ -0,0 +1,19 @@
+/*Enter your code here. Read input from STDIN. Print your output to STDOUT*/
+#include <iostream>
+using namespace std;
+
+int gcd(int a, int b) {
+   if (b == 0)
+ 		return a;
+   return gcd(b, a % b);
+}
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+
+	cout<<gcd(a,b);
+
+	return 0;
+}
